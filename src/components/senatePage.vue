924025f54cccf67fa98cbcc0eab58500dc17c700
papiricoh <slendeman0200@gmail.com>
2023-04-14 11:13:45 +0200

    added if text editor

--- src/components/senatePage.vue
+++ src/components/senatePage.vue
@@ -11,6 +11,7 @@ export default {
   },
   data() {
     return {
+      textEditorOn: false,
       pending_laws: [
         { title: "Minecraft Bill", type: "Bill", votes: [
           { senator_id: "steam:2039120", vote: "for"},
@@ -49,45 +50,50 @@ export default {
 
 <template>
   <div>
-    <div class="senate_page_heading">
-      <h2>Pending laws overview</h2>
-      <button style="margin: 1.4rem;">New Law</button>
-    </div>
-    <div class="pending_laws_table">
-      <table>
-        <tr class="pending_laws_table_row">
-          <th>Law name</th>
-          <th>Law type</th>
-          <th>For - Neutral - Against</th>
-          <th>Open vote until</th>
-          <th>Options</th>
-        </tr>
-        <tr class="pending_laws_table_row" v-for="law in pending_laws">
-          <td>{{law.title}}</td>
-          <td>{{law.type}}</td>
-          <td><voteUI @updatePendingLaws="updatePendingLaws($event)" :player="player" :law="law"></voteUI></td>
-          <td>{{new Date(law.time).toDateString()}}</td>
-          <td><button>Read</button></td>
-        </tr>
-      </table>
-    </div>
-    <h2>Parties overview</h2>
-    <div class="parties_seccion">
-      <div class="party_card" v-for="party in parties">
-        <h4 :style="'color: ' + party.color + ';'">{{party.name}}</h4>
-        <div class="party_table">
-          <table>
-            <tr>
-              <th>Name</th>
-              <th>Role</th>
-            </tr>
-            <tr v-for="member in party.members">
-              <td>{{member.name}}</td>
-              <td>{{member.role}}</td>
-            </tr>
-          </table>
+    <div>
+      <div class="senate_page_heading">
+        <h2>Pending laws overview</h2>
+        <button style="margin: 1.4rem;">New Law</button>
+      </div>
+      <div class="pending_laws_table">
+        <table>
+          <tr class="pending_laws_table_row">
+            <th>Law name</th>
+            <th>Law type</th>
+            <th>For - Neutral - Against</th>
+            <th>Open vote until</th>
+            <th>Options</th>
+          </tr>
+          <tr class="pending_laws_table_row" v-for="law in pending_laws">
+            <td>{{law.title}}</td>
+            <td>{{law.type}}</td>
+            <td><voteUI @updatePendingLaws="updatePendingLaws($event)" :player="player" :law="law"></voteUI></td>
+            <td>{{new Date(law.time).toDateString()}}</td>
+            <td><button>Read</button></td>
+          </tr>
+        </table>
+      </div>
+      <h2>Parties overview</h2>
+      <div class="parties_seccion">
+        <div class="party_card" v-for="party in parties">
+          <h4 :style="'color: ' + party.color + ';'">{{party.name}}</h4>
+          <div class="party_table">
+            <table>
+              <tr>
+                <th>Name</th>
+                <th>Role</th>
+              </tr>
+              <tr v-for="member in party.members">
+                <td>{{member.name}}</td>
+                <td>{{member.role}}</td>
+              </tr>
+            </table>
+          </div>
         </div>
       </div>
+    </div>
+    <div v-if="textEditorOn">
+      
     </div>
   </div>
 </template>
