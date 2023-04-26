08ec17ffa5c5db3636fc75576696e21e8d6666f8
Papiricoh <slendeman0200@gmail.com>
2023-04-26 02:45:20 +0200

    added law system

--- src/components/lawsPage.vue
+++ src/components/lawsPage.vue
@@ -9,17 +9,27 @@ export default {
   },
   data() {
     return {
+      law_page: "Constitution",
       laws: [
         { title: "Los Santos Constitution", type: "Constitution", time: 1681442485000, jurisdiction: "Los Santos", ideology: "Center", body: "<p>Law text</p>"},
         { title: "Minecraft Bill", type: "Bill", time: 1681442485000, jurisdiction: "Los Santos", ideology: "Center", body: "<p>Law text</p>"},
+        { title: "d Bill", type: "Bill", time: 1681442485000, jurisdiction: "Los Santos", ideology: "Center", body: "<p>Law text</p>"},
       ],
+      displayed_laws: [],
     };
   },
-  mounted() {
-    
-  },
   methods: {
-
+    updateLawList() {
+      this.displayed_laws = [];
+      for (let index = 0; index < this.laws.length; index++) {
+        if(this.laws[index].type == this.law_page) {
+          this.displayed_laws[this.displayed_laws.length] = this.laws[index];
+        }
+      }
+    }
+  },
+  mounted() {
+    this.updateLawList();
   },
   components: {
     
@@ -36,8 +46,26 @@ export default {
     <div class="laws_body">
       <div class="laws_seccion">
         <div class="laws_seccion_menu">
-          <div class="laws_menu_button" v-for="law in laws">
-            <button>{{law.title}}</button>
+          <div class="laws_menu_button">
+            <button class="active_button" v-if="law_page == 'Constitution'">Constitution</button>
+            <button @click="law_page = 'Constitution', updateLawList()" v-else>Constitution</button>
+          </div>
+          <div class="laws_menu_button">
+            <button class="active_button" v-if="law_page == 'amendment'">Amendment</button>
+            <button @click="law_page = 'amendment', updateLawList()" v-else>Amendment</button>
+          </div>
+          <div class="laws_menu_button">
+            <button class="active_button" v-if="law_page == 'Bill'">Bill</button>
+            <button @click="law_page = 'Bill', updateLawList()" v-else>Bill</button>
+          </div>
+          <div class="laws_menu_button">
+            <button class="active_button" v-if="law_page == 'Regional Law'">Regional Law</button>
+            <button @click="law_page = 'Regional Law', updateLawList()" v-else>Regional Law</button>
+          </div>
+        </div>
+        <div class="laws_seccion_body">
+          <div v-for="law in displayed_laws" class="law_container">
+            <div>{{ law.title }}</div>
           </div>
         </div>
       </div>
